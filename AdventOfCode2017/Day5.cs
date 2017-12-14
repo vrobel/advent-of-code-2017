ca96dd0e51357ad6a12a6c369dce69ff831b8c89
Michal Wroblewski <michal.wroblewski@setapp.pl>
2017-12-15 00:05:31 +0100

    Working Day5 (first part)

---
+++ AdventOfCode2017/Day5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace AdventOfCode2017
+{
+    [TestFixture]
+    public class Day5
+    {
+        private int[] _input;
+
+        [SetUp]
+        public void LoadInput()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory,
+                "AdventOfCode2017/Resources/day5_input.txt");
+            var lines = File.ReadAllLines(path);
+            _input = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                _input[i] = Int32.Parse(lines[i]);
+            }
+        }
+
+        [Test]
+        public void TestMaze()
+        {
+            var exampleInput = new[] {0, 3, 0, 1, -3};
+            Assert.AreEqual(5, CountMazeSteps(exampleInput));
+
+            Console.WriteLine(CountMazeSteps(_input));
+        }
+
+        private int CountMazeSteps(int[] input)
+        {
+            var inputSize = input.Length;
+            var visitedOffset = new uint[inputSize];
+            var i = 0;
+            var iterations = 0;
+            while (i >= 0 && i < inputSize)
+            {
+                var offset = (int) visitedOffset[i];
+                visitedOffset[i]++;
+                i += input[i] + offset;
+                iterations++;
+            }
+            return iterations;
+        }
+    }
+}
