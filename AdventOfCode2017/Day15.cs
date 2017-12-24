3fd7e99ae6896914fe4742ef06455a16fc0a921a
Michał Wróblewski <michal.wroblewski@setapp.pl>
2017-12-24 15:07:33 +0100

    Day 15, second part

--- AdventOfCode2017/Day15.cs
+++ AdventOfCode2017/Day15.cs
@@ -8,6 +8,7 @@ namespace AdventOfCode2017
     {
         private const int EndingBitMask = 0xFFFF;
         private const int Iterations = 40_000_000;
+        private const int PerkyIterations = 5_000_000;
 
         private const int GenAMultiplier = 16807;
         private const int GenBMultiplier = 48271;
@@ -34,6 +35,24 @@ namespace AdventOfCode2017
             Assert.AreEqual(1431495498, generator.NextValue());
             Assert.AreEqual(137874439, generator.NextValue());
             Assert.AreEqual(285222916, generator.NextValue());
+        }
+
+        [Test]
+        public void TestExamplePerkyGenerator()
+        {
+            var generator = new PerkyGenerator(ExampleGenA, GenAMultiplier, 4);
+            Assert.AreEqual(1352636452, generator.NextValue());
+            Assert.AreEqual(1992081072, generator.NextValue());
+            Assert.AreEqual(530830436, generator.NextValue());
+            Assert.AreEqual(1980017072, generator.NextValue());
+            Assert.AreEqual(740335192, generator.NextValue());
+            
+            generator = new PerkyGenerator(ExampleGenB, GenBMultiplier, 8);
+            Assert.AreEqual(1233683848, generator.NextValue());
+            Assert.AreEqual(862516352, generator.NextValue());
+            Assert.AreEqual(1159784568, generator.NextValue());
+            Assert.AreEqual(1616057672, generator.NextValue());
+            Assert.AreEqual(412269392, generator.NextValue());
         }
 
         [Test]
@@ -65,13 +84,23 @@ namespace AdventOfCode2017
 
             Console.WriteLine($"Generator results matched {count} times");
         }
+
+        [Test]
+        public void Test40MillionPerkyIterations()
+        {
+            var genA = new PerkyGenerator(InputGenA, GenAMultiplier, 4);
+            var genB = new PerkyGenerator(InputGenB, GenBMultiplier, 8);
+            var count = CountMatchesOverIteration(genA, genB, PerkyIterations);
+
+            Console.WriteLine($"Generator results matched {count} times");
+        }
         
         private bool CompareGeneratorResults(int a, int b)
         {
             return (a & EndingBitMask) == (b & EndingBitMask);
         }
 
-        private int CountMatchesOverIteration(Generator genA, Generator genB, int iterations)
+        private int CountMatchesOverIteration(IGenerator genA, IGenerator genB, int iterations)
         {
             int count = 0;
             for (int i = 0; i < iterations; i++)
@@ -83,8 +112,13 @@ namespace AdventOfCode2017
             }
             return count;
         }
+        
+        private interface IGenerator
+        {
+            int NextValue();
+        }
 
-        private class Generator
+        private class Generator : IGenerator
         {
             private long _current;
             private readonly int _multiplier;
@@ -102,5 +136,27 @@ namespace AdventOfCode2017
             }
         }
 
+        private class PerkyGenerator : IGenerator
+        {
+            private long _current;
+            private readonly int _multiplier;
+            private readonly int _acceptedDivider;
+
+            public PerkyGenerator(int init, int multiplier, int acceptedDivider)
+            {
+                _current = init;
+                _multiplier = multiplier;
+                _acceptedDivider = acceptedDivider;
+            }
+
+            public int NextValue()
+            {
+                do
+                {
+                    _current = _current * _multiplier % int.MaxValue;
+                } while (_current % _acceptedDivider != 0);
+                return (int) _current;
+            }
+        }
     }
 }
